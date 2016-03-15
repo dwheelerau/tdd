77d93800db34c65fc517f79dce30061eb9f234bf
Dave Wheeler <dwheelerau@gmail.com>
2016-03-15 21:26:23 +1300

    view returns basic html

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
+from django.http import HttpRequest
+
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -9,3 +11,12 @@ class HomePageTest(TestCase):
         # resolve is a django funct to resolve urls
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        # http req obj when browswer ask for page
+        request = HttpRequest()
+        # pass it to homepage, that returns a response
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
