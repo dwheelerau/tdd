d438e741907ad6219457b3b3d07d2c46bce8bea6
Dave Wheeler <dwheelerau@gmail.com>
2016-03-16 21:00:42 +1300

    pg 58 running

--- lists/tests.py
+++ lists/tests.py
@@ -20,3 +20,20 @@ class HomePageTest(TestCase):
         response = home_page(request)
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        # this just get the post that is name='item_text' in home.html
+        # it is also just pretending to be user in this case
+        # basically we are saying post a string in this form
+        request.POST['item_text'] = 'A new list item'
+        response = home_page(request)
+
+        # this just makes sure that the response comes back
+
+        print(response.content.decode())
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html', {'new_item_text': 'A new list item'})
+        self.assertEqual(response.content.decode(), expected_html)
--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'home.html')
+    # this takes the val from the test scirpt and insert it in the home
+    # page
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+        })
