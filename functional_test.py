e027772b46283900765283d706e01a42df6e080e
Dave Wheeler <dwheelerau@gmail.com>
2016-03-16 19:19:37 +1300

    Front page html now gen template

--- functional_test.py
+++ functional_test.py
@@ -7,7 +7,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(5)
 
     def tearDown(self):
         self.browser.quit()
@@ -20,7 +20,8 @@ class NewVisitorTest(unittest.TestCase):
         # she notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
+        # changed from page 38 of book from To-Do
+        self.assertIn('Your', header_text)
 
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
