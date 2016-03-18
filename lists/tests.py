0dc0ee8b6e29469722e0cfc9585af30884e5e756
Dave Wheeler <dwheelerau@gmail.com>
2016-03-18 19:15:52 +1300

    model for list items and ass migrations

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.core.urlresolvers import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from lists.models import Item
 
 
 # Create your tests here.
@@ -32,8 +33,28 @@ class HomePageTest(TestCase):
 
         # this just makes sure that the response comes back
 
-        print(response.content.decode())
         self.assertIn('A new list item', response.content.decode())
         expected_html = render_to_string(
             'home.html', {'new_item_text': 'A new list item'})
         self.assertEqual(response.content.decode(), expected_html)
+
+    def test_saving_and_retrieving_items(self):
+        # classes are dbs, create hear see lists.models for class defn
+        first_item = Item()
+        # instances are cols ie text column
+        first_item.text = 'The first (ever) list item'
+        # create object at this row
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+        # use .objects to acc all data, returns list like obj
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        # check to see if db object returns is recovered correctly
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
