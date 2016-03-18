cc6af4b9e71f7be8b32833cd2cb9cda38015fbb7
Dave Wheeler <dwheelerau@gmail.com>
2016-03-19 09:11:03 +1300

    end chapter 5

--- functional_test.py
+++ functional_test.py
@@ -36,16 +36,20 @@ class NewVisitorTest(unittest.TestCase):
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
-        inputbox.send_keys('Buy peacock feathers')
-
-        # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        # When she hits enter, the page updates, and now the page lists
 
         # There is still a text box inviting her to add another item. She
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
         self.check_for_row_in_list_table(
             '2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # enters "Use peac feathers to make a fly" (Edith is very methodical)
         self.fail('finish the tests')
--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from lists.models import Item
 
 
 # Create your views here.
 def home_page(request):
     # this takes the val from the test scirpt and insert it in the home
     # page
-    return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
-        })
+    # save object in database, tests.py recovers
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
