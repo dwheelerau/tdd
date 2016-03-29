3ef609064b965bddc76459415ba623724df4ba21
Dave Wheeler <dwheelerau@gmail.com>
2016-03-29 15:11:42 +1300

    pg 96

--- lists/tests.py
+++ lists/tests.py
@@ -23,32 +23,6 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
 
-    def test_home_page_can_save_a_POST_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        # this just get the post that is name='item_text' in home.html
-        # it is also just pretending to be user in this case
-        # basically we are saying post a string in this form
-        request.POST['item_text'] = 'A new list item'
-
-        response = home_page(request)
-
-        # this just makes sure that the response comes back
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_home_page_redirects_after_POST(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
-
-        response = home_page(request)
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(
-            response['location'], '/lists/the-only-list-in-the-world/')
-
     def test_saving_and_retrieving_items(self):
         # classes are dbs, create hear see lists.models for class defn
         first_item = Item()
@@ -70,11 +44,6 @@ class HomePageTest(TestCase):
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
 
-    def test_home_page_only_saves_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(), 0)
-
 
 class ListViewTest(TestCase):
 
@@ -91,3 +60,25 @@ class ListViewTest(TestCase):
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+
+
+class NewListTest(TestCase):
+
+    def test_saving_a_POST_request(self):
+        self.client.post(
+            '/lists/new',
+            data={'item_text': 'A new list item'}
+        )
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+
+        response = self.client.post(
+            '/lists/new',
+            data={'item_text': 'A new list item'}
+        )
+
+        self.assertRedirects(
+            response, '/lists/the-only-list-in-the-world/')
--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import redirect, render
 from lists.models import Item
 
@@ -7,12 +8,14 @@ def home_page(request):
     # this takes the val from the test scirpt and insert it in the home
     # page
     # save object in database, tests.py recovers
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
