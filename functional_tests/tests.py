40a485ce9de61d9a53f68aa0e85bdee62386f98b
Dave Wheeler <dwheelerau@gmail.com>
2016-04-02 11:29:45 +1300

    added STATIC root

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -86,6 +86,7 @@ class NewVisitorTest(LiveServerTestCase):
         # edith goes to the home page
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
+        print(self.live_server_url)
 
         # she notices the infput box nicely centered
         inputbox = self.browser.find_element_by_id('id_new_item')
