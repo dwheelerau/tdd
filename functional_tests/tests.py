d870ba39041d37d7781bd8c5342c6e6cc5b6947a
Dave Wheeler <dwheelerau@gmail.com>
2016-04-09 09:48:00 +1200

    moved database to ../database dir

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import sys
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -21,7 +36,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. she goes
         # to check out its homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # she notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -61,7 +76,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Firefox()
 
         # Francis visits the home page, no sign of ediths lists
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -84,9 +99,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # satisfied they both go back to sleep
     def test_layout_and_styling(self):
         # edith goes to the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
-        print(self.live_server_url)
+        print(self.server_url)
 
         # she notices the infput box nicely centered
         inputbox = self.browser.find_element_by_id('id_new_item')
