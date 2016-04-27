a051e7076f7c7fc0a5eeabe65558de7bc11d3a4d
Dave Wheeler <dwheelerau@gmail.com>
2016-04-28 07:02:23 +1200

    refrac and using form views, working state

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -66,7 +66,6 @@ class ListViewTest(TestCase):
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
     def test_passes_correct_list_to_template(self):
-        other_list = List.objects.create()
         correct_list = List.objects.create()
         response = self.client.get('/lists/%d/' % (correct_list.id,))
         self.assertEqual(response.context['list'], correct_list)
@@ -76,6 +75,7 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/%d/' % (list_.id,))
         self.assertIsInstance(response.context['form'], ItemForm)
         self.assertContains(response, 'name="text"')
+
 
 class NewListTest(TestCase):
 
@@ -104,7 +104,6 @@ class NewListTest(TestCase):
         self.assertEqual(Item.objects.count(), 0)
 
     def test_can_save_a_POST_request_to_an_existing_list(self):
-        other_list = List.objects.create()
         correct_list = List.objects.create()
 
         self.client.post(
@@ -118,7 +117,6 @@ class NewListTest(TestCase):
         self.assertEqual(new_item.list, correct_list)
 
     def test_POST_redirects_to_list_view(self):
-        other_list = List.objects.create()
         correct_list = List.objects.create()
 
         response = self.client.post(
